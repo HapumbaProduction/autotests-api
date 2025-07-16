679b26985a480b7e715ed82fa36579234bc1ab1d
HapumbaProduction <kaban050411@gmail.com>
2025-07-16 18:19:03 +0300

    Знакомство с Faker. Применение Fake в Pydantic-моделях

--- clients/authentication/authentication_schema.py
+++ clients/authentication/authentication_schema.py
@@ -1,36 +1,38 @@
 from pydantic import BaseModel, Field
 
+from tools.fakers import fake
 
-# Добавили суффикс Schema вместо Dict
-class TokenSchema(BaseModel):  # Наследуем от BaseModel вместо TypedDict
+
+class TokenSchema(BaseModel):
     """
     Описание структуры аутентификационных токенов.
     """
-    token_type: str = Field(alias="tokenType")  # Использовали alise
-    access_token: str = Field(alias="accessToken")  # Использовали alise
-    refresh_token: str = Field(alias="refreshToken")  # Использовали alise
+
+    token_type: str = Field(alias="tokenType")
+    access_token: str = Field(alias="accessToken")
+    refresh_token: str = Field(alias="refreshToken")
 
 
-# Добавили суффикс Schema вместо Dict
-class LoginRequestSchema(BaseModel):  # Наследуем от BaseModel вместо TypedDict
+class LoginRequestSchema(BaseModel):
     """
     Описание структуры запроса на аутентификацию.
     """
-    email: str
-    password: str
+
+    email: str = Field(default_factory=fake.email)
+    password: str = Field(default_factory=fake.password)
 
 
-# Добавили суффикс Schema вместо Dict
-class LoginResponseSchema(BaseModel):  # Наследуем от BaseModel вместо TypedDict
+class LoginResponseSchema(BaseModel):
     """
     Описание структуры ответа аутентификации.
     """
+
     token: TokenSchema
 
 
-# Добавили суффикс Schema вместо Dict
-class RefreshRequestSchema(BaseModel):  # Наследуем от BaseModel вместо TypedDict
+class RefreshRequestSchema(BaseModel):
     """
     Описание структуры запроса для обновления токена.
     """
-    refresh_token: str = Field(alias="refreshToken")  # Использовали alise
+
+    refresh_token: str = Field(alias="refreshToken")
